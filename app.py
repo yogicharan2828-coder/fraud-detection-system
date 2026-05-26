e4e0005e4ad8d00a3e55674dce7f40bf07459705
yogicharan <yogicharan2828@gmail.com>
2026-05-26 16:27:12 +0530

    fixed fraud detection logic

--- app.py
+++ app.py
@@ -2,26 +2,39 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Home Page
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
+# Prediction Route
 @app.route('/predict', methods=['POST'])
 def predict():
 
+    # Get values from form
+    transaction_type = request.form['type']
     amount = float(request.form['amount'])
-oldbalance = float(request.form['oldbalance'])
-newbalance = float(request.form['newbalance'])
+    oldbalance = float(request.form['oldbalance'])
+    newbalance = float(request.form['newbalance'])
 
-if amount > oldbalance:
-    result = "Fraud"
-elif amount > 50000:
-    result = "Fraud"
-else:
-    result = "Not Fraud"
+    # Fraud Detection Logic
+    if amount > oldbalance:
+        prediction = "Fraud"
 
-return render_template("index.html", prediction=result)
-    
+    elif amount > 50000:
+        prediction = "Fraud"
 
+    else:
+        prediction = "Not Fraud"
+
+    # Send result back to webpage
+    return render_template(
+        'index.html',
+        prediction=prediction
+    )
+
+
+# Run Flask App
 if __name__ == "__main__":
     app.run(debug=True)
